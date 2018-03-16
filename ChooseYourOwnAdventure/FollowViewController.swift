0e639e6c3103d7e645ef8d37800ea32757cd26ca
Maribel Montejano <outdoorsole@users.noreply.github.com>
2018-03-15 23:25:00 -0700

    Pass data to FollowVC

--- ChooseYourOwnAdventure/FollowViewController.swift
+++ ChooseYourOwnAdventure/FollowViewController.swift
@@ -12,10 +12,20 @@ class FollowViewController: UIViewController {
 
     var name: String?
     
+    @IBOutlet weak var firstChoiceButton: UIButton!
+    @IBOutlet weak var secondChoiceButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if name != nil {
+            firstChoiceButton.setTitle("\(name!), do you decide to follow them inside?", for: UIControlState.normal)
+            secondChoiceButton.setTitle("\(name!), do you decide to turn back, forget it all, and go on your way to school?", for: UIControlState.normal)
+        } else {
+            firstChoiceButton.setTitle("Do you decide to follow them inside?", for: UIControlState.normal)
+            secondChoiceButton.setTitle("Do you decide to turn back, forget it all, and go on your way to school?", for: UIControlState.normal)
+        }
     }
 
 }
